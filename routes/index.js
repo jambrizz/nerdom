08e786b26a8f70a58fc181ee4fd9ffc2f508e4df
jbetz08 <jbetzsold28@gmail.com>
2022-11-17 11:39:01 -0700

    swagger docs

--- routes/index.js
+++ routes/index.js
@@ -1,4 +1,6 @@
 const express = require('express');
 const router = express.Router();
 
+router.use('/api-docs', require('./docs'));
+
 module.exports = router;
---
+++ swagger.js
@@ -0,0 +1,16 @@
+const swaggerAutogen = require('swagger-autogen')();
+
+const doc = {
+  info: {
+    title: 'Nerdom',
+    description: 'an app for nerds',
+  },
+  host: 'localhost:3000',
+  schemes: ['http'],
+};
+
+const outputFile = './swagger-output.json';
+const endpointsFiles = ['./app.js'];
+
+
+swaggerAutogen(outputFile, endpointsFiles, doc);
